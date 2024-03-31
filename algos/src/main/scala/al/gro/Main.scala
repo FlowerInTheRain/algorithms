8cb38e88fa2bfdb8b6464f54d50b38ef5c29b546
Sid-Athos <sa.bennaceur@gmail.com>
2024-03-31 13:52:53 +0200

    lul
    Took 14 minutes

--- algos/src/main/scala/al/gro/Main.scala
+++ algos/src/main/scala/al/gro/Main.scala
@@ -1,13 +1,12 @@
 package al.gro
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    var i = 88;
-    for(n <- 0 to i.toString.length){
-     i =i.toString().split("").map(_.toInt).sum
+    def main(args: Array[String]): Unit = {
+        var i = 88;
+        for (n <- 0 to i.toString.length) {
+            i = i.toString().split("").map(_.toInt).sum
 
+        }
+        println(i)
     }
-    println(i)
-
-  }
 }
